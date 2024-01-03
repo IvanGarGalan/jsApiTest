c51a0c9f62601d800846701588c1540e78025fa1
IvanGarGalan <123881940+IvanGarGalan@users.noreply.github.com>
2024-01-03 12:05:01 +0100

    structure and api call
    the apis are called, errors handled and the basic structure of the app is now ready

--- js/script.js
+++ js/script.js
@@ -1,51 +1,65 @@
-// const url = 'https://dad-jokes7.p.rapidapi.com/dad-jokes/search?text=dad';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'b30d4270e1msh380f183148f4235p16466cjsnd19a79f41908',
-// 		'X-RapidAPI-Host': 'dad-jokes7.p.rapidapi.com'
-// 	}
-// };
+let btnTranslate = document.getElementById('btnTranslate')
+let textTranslate = document.getElementById('textTranslate')
+let text = document.getElementById('text')
 
-// const urlText = 'https://text-translator2.p.rapidapi.com/translate';
-// const optionsText = {
-// 	method: 'POST',
-// 	headers: {
-// 		'content-type': 'application/x-www-form-urlencoded',
-// 		'X-RapidAPI-Key': 'b30d4270e1msh380f183148f4235p16466cjsnd19a79f41908',
-// 		'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
-// 	},
-// 	body: new URLSearchParams({
-// 		source_language: 'en',
-// 		target_language: 'id',
-// 		text: 'What is your name?'
-// 	})
-// };
+const traducirTexto = async (wordToTranslate) =>{
+    try {
+        const response = await fetch('https://text-translator2.p.rapidapi.com/translate', optionsText = {
+    method: 'POST',
+	headers: {
+		'content-type': 'application/x-www-form-urlencoded',
+		'X-RapidAPI-Key': 'b30d4270e1msh380f183148f4235p16466cjsnd19a79f41908',
+		'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+	},
+	body: new URLSearchParams({
+		source_language: 'es',
+		target_language: 'en',
+		text: wordToTranslate
+	})
+        });
 
-// const cargarTexto = async () =>{
-//     try {
-//         const response = await fetch(urlText, optionsText);
-//         const result = await response.json();
-//         console.log(result.data.translatedText);
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
+        const result = await response.json();
+        // console.log(result.data.translatedText);
+        cargarChiste(result.data.translatedText)
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 
 
-// const cargarChiste = async () =>{
-//     try {
-//         const response = await fetch(url, options);
-//         const result = await response.json();
-//         console.log(result.data.translatedText);
-//     } catch (error) {
-//         console.error(error);
-//     }
+const cargarChiste = async (wordSearch) =>{
+    try {
+        const response = await fetch(`https://dad-jokes7.p.rapidapi.com/dad-jokes/search?text=${wordSearch}`, options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'b30d4270e1msh380f183148f4235p16466cjsnd19a79f41908',
+                'X-RapidAPI-Host': 'dad-jokes7.p.rapidapi.com'
+            }
+        });
+        const result = await response.json();
+        //para cuando se acaben las pruebas
+        if(response.status == 429){
+            text.textContent = 'Se ha consumido la cuota diaria'
+        }else{
+            mostrarResultado(result);
+        }
+    } catch (error) {
+        console.log(error);
+    }
 
-// }
+}
+
+const mostrarResultado = (event) =>{
+    text.textContent = event
+}
+
+const translateInput = () =>{
+    let translation = textTranslate.value 
+    // console.log(translation)
+    traducirTexto(translation)
+}
 
 
 
-// document.addEventListener("DOMContentLoaded",cargarDatos)
-// document.addEventListener("DOMContentLoaded",cargarTexto) 
+btnTranslate.addEventListener("click",translateInput)
